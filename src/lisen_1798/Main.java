19b91201b7f75ca820fe50db3248a2dc3f3fb6a3
Skaredin <sksv94@mail.ru>
2024-05-01 19:20:56 +0500

    2

--- src/lisen_1798/Main.java
+++ src/lisen_1798/Main.java
@@ -4,24 +4,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Ввод радиуса огненного круга
         long R = scanner.nextLong();
 
-        // Вычисление количества плит внутри круга
         long innerPlates = (long) Math.floor(Math.PI * R * R);
-
-        // Вычисление количества плит на расстоянии от R до sqrt(2)*R
         long outerPlates = (4 * countPlatesInQuarter(R))+2;
-
-        // Вычисление общего количества испорченных плит
         long totalPlates = innerPlates + outerPlates;
 
-        // Вывод результата
         System.out.println(totalPlates);
     }
 
-    // Метод для подсчёта количества плит в одной четверти круга
+
     private static long countPlatesInQuarter(long R) {
         long count = 0;
         for (long x = R + 1; x * x <= 2 * R * R; x++) {
